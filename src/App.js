8fea872a41713532d09270a592738873d79b5ffc
Casey MacLeod <caseymacleod22@gmail.com>
2022-08-19 23:31:48 -0400

    starts building out components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+const numDice = 5
+const numRolls = 3
+
+function Game() {
+  const [gameState, setGameState] = useState({
+    dice: Array.from({ length: numDice }),
+    locked: Array(numDice).fill(false),
+    rollsLeft: numRolls,
+    rolling: false,
+    scores: {
+      ones: undefined,
+      twos: undefined,
+      threes: undefined,
+      fours: undefined,
+      fives: undefined,
+      sixes: undefined,
+      threeOfKind: undefined,
+      fourOfKind: undefined,
+      fullHouse: undefined,
+      smallStraight: undefined,
+      largeStraight: undefined,
+      yahtzee: undefined,
+      chance: undefined
+    }
+  })
+}
 
 function App() {
   return (
